c04545c345169d217c0fa3278c498dfab04193a8
iBoyIndustry <125243686+iBoyIndustry@users.noreply.github.com>
2023-02-13 14:21:20 +0700

    Add files via upload

---
+++ Muchlisin.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package muchlisin;
+
+/**
+ *
+ * @author User
+ */
+public class Muchlisin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("E41221859");
+        System.out.println("NUR MUCHLISIN DWI PUTRO");
+        System.out.println("Golongan B");
+    }
+    
+}
